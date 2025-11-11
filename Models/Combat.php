0401c7fd7f6d96212ca9afd5362c3e402430176e
Gabriel Ceron Bianchi <431191@unoesc.lan>
2025-11-10 22:03:59 -0300

    ĆombatWIP

--- Models/Combat.php
+++ Models/Combat.php
@@ -1,3 +1,18 @@
 <?php
 
-class Combat {}
+require_once 'Dice.php';
+
+class Combat
+{
+    public static function determinarIniciativa(Character $p1, Character $p2)
+    {
+        $rolagemP1 = Dice::lancar(20) + $p1->getVelocidadeTotal;
+        $rolagemP2 = Dice::lancar(20) + $p2->getVelocidadeTotal;
+
+        if ($rolagemP1 > $rolagemP2) {
+            return $p1;
+        } else {
+            return $p2;
+        }
+    }
+}
--- Models/Dice.php
+++ Models/Dice.php
@@ -1,3 +1,37 @@
 <?php
 
-class Dice {}
+class Dice
+{
+    public static function lancar(int $lados)
+    {
+        if ($lados < 2) {
+            return "Escolha um dado com no mínimo 2 lados";
+        } else {
+            $num_aleatorio = rand(1, $lados);
+            return $num_aleatorio;
+        }
+    }
+
+    public static function lancarMultiplos(int $lados, int $vezes)
+    {
+        $soma = 0;
+        if ($vezes < 1) {
+            return "Digite um número válido para as tentativas <br>";
+        } else {
+            for ($i = 0; $i < $vezes; $i++) {
+                $soma += Dice::lancar($lados);
+            }
+            return $soma;
+        }
+    }
+
+    public static function gerarResultado(int $min, int $max): int
+    {
+        if ($min > $max) {
+            $temp = $min;
+            $min = $max;
+            $max = $temp;
+        }
+        return rand($min, $max);
+    }
+}
